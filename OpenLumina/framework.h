28439ea12fd8f498db1fba182c63dabcc53eaf3f
TOM_RUS <tomrus88@gmail.com>
2024-03-28 12:50:13 +0300

    Fix

--- OpenLumina/framework.h
+++ OpenLumina/framework.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#if WIN32
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
 #include <Wincrypt.h>
+
+#endif
