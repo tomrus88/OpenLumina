4948924dbf27dd008fc18adc20ee38fa1ab69bda
TOM_RUS <tomrus88@gmail.com>
2024-03-30 09:08:58 +0300

    Fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cmake -DIdaSdk_ROOT_DIR=/home/tom_rus/idasdk_pro84 -B out
+cmake -DCMAKE_BUILD_TYPE=Release -DIdaSdk_ROOT_DIR=/home/tom_rus/idasdk_pro84 -B out
 cmake --build out
 cp out/OpenLumina64.so /home/tom_rus/idafree-8.4/plugins/
