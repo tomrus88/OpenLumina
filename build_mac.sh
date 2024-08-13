044add069535988d4c78c59fac64e713ff94d2d2
TOM_RUS <tomrus88@gmail.com>
2024-08-14 00:01:11 +0300

    Fix

--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,3 @@
-cmake -DCMAKE_BUILD_TYPE=Release -DIdaSdk_ROOT_DIR=/Users/tom_rus/Desktop/idasdk_pro84 -B out
+cmake -DCMAKE_BUILD_TYPE=Release -DIdaSdk_ROOT_DIR=/Users/tom_rus/Desktop/idasdk90 -B out
 cmake --build out
 cp out/OpenLumina64.dylib "/Applications/IDA Freeware 8.4/ida64.app/Contents/MacOS/plugins/"
