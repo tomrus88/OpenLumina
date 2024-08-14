5a297494e9a64f14400b950c5573e5099ebec5e7
TOM_RUS <tomrus88@gmail.com>
2024-08-14 18:08:21 +0300

    Fix

--- OpenLumina/plthook/plthook_osx.c
+++ OpenLumina/plthook/plthook_osx.c
@@ -1130,6 +1130,8 @@ matched:
             if (dladdr((void*)addr, &dli6))
                 fprintf(stderr, "base of %s %s %p\n", funcname, dli6.dli_fname, dli6.dli_fbase);
 
+            addr += 0x30;
+
             fprintf(stderr, "replacing %p with %p at %p\n", *addr, funcaddr, addr);
             *addr = funcaddr;
             mprotect(base, page_size, prot);
