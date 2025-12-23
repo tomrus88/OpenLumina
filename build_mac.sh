412b34551dc06556d2d545f3e679a34067da3d58
TOM_RUS <tomrus88@users.noreply.github.com>
2025-12-23 05:06:32 +0300

    Fix

--- build_mac.sh
+++ build_mac.sh
@@ -2,4 +2,4 @@ rm -r out
 cmake -DCMAKE_BUILD_TYPE=Release -DIdaSdk_ROOT_DIR=/Users/tom_rus/Desktop/idasdk91 -DIDA_90_STABLE=1 -B out
 cmake --build out
 zip -j openlumina_mac.zip out/OpenLumina*.dylib
-cp out/OpenLumina64.dylib "/Applications/IDA Freeware 8.4/ida64.app/Contents/MacOS/plugins/"
+#cp out/OpenLumina64.dylib "/Applications/IDA Freeware 8.4/ida64.app/Contents/MacOS/plugins/"
